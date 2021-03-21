a9e1871fd1f25507f0369718ed030cd8d0b40881
osaze <osareniyeosazee@gmail.com>
2021-03-21 01:21:40 +0100

    tru profile

---
+++ src/components/Footer.js/Footer.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from "./footer.module.css"
+
+export default function Footer() {
+    return (
+        <div className={styles.footerhomew}>
+              <footer className={styles.footer}>
+          <section>
+            <h2>
+              <i className='fab fa-twitter'></i> Follow top activists
+            </h2>
+            <ul>
+             
+              <li>
+                <a
+                 href='https://github.com/eddynice'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                  @Eddynice
+                </a>
+              </li>
+             
+              
+            </ul>
+          </section>
+
+       {/*    <section>
+            <h2>
+              <i className='fas fa-tshirt'></i> Buy an #EndSARS T-shirt
+            </h2>
+            <img className={styles.ad} src='assets/img/ad.jpg' alt='' />
+       </section>  */}
+
+          <section>
+            <h2>
+              <i className='fas fa-laptop-code'></i> Contributor
+            </h2>
+            <ul>
+              <li>
+                <a
+                  href='https://twitter.com/iamEddynics'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                 @iamEddynics
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href='https://twitter.com/'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                  @iamEddynics
+                </a>
+              </li>
+
+              
+
+             
+              <li>
+                <a
+                  href='https://twitter.com/ridwanabiola2'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                  @tru
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href='https://twitter.com/EvidSO'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                  @EvidenceISO
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href='https://twitter.com/'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                  
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href='https://twitter.com/'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                  @walebant
+                </a>
+              </li>
+
+             
+
+            </ul>
+          </section>
+
+
+          <section>
+            <h2>
+              <i className='fab fa-twitter'></i> About me
+            </h2>
+            <ul>
+             
+              <li>
+                
+                <Link to="/about">tru</Link>
+                  
+               
+              </li>
+             
+              
+            </ul>
+          </section>
+        </footer>
+        </div>
+    )
+}
